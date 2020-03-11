38dc177f90a6782d709a2ab82d2a1ad9192f1146
taoqf <tao_qf@163.com>
2020-03-11 17:15:55 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import moment from 'moment';
 
-export default function an10(param1: string, param2: string) {
+/**
+ * 日期类型转换成时间戳
+ * @param time 日期
+ * @param format 字符串的格式
+ */
+export default function parse(time: string, format: string) {
+	return moment(time, format).valueOf();
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,14 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('日期类型转换成时间戳 YYYY/MM/DD', (t) => {
+	t.is(a('2020/01/01', 'YYYY/MM/DD'), 1577808000000);
+});
+
+test('日期类型转换成时间戳 YYYY-MM-DD', (t) => {
+	t.is(a('2020-01-01', 'YYYY-MM-DD'), 1577808000000);
+});
+
+test('日期类型转换成时间戳 YYYY-M-D', (t) => {
+	t.is(a('2020-1-1', 'YYYY-M-D'), 1577808000000);
 });
